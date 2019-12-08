0ad0b40dae5d03a48dfa75d5a341d3525f54decd
rickyjames1750 <rickyjames1750@gmail.com>
2019-12-08 00:58:57 -0800

    refactor: added useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React,{ useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = "c21c1abe";
   const APP_KEY = "57187b2ead30d513f516c237b4d5c887";
+ const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  }, []);
+
   return(
     <div className="App">
       <form className="search-form">
